cfa539664591a49b7bbd075845a7f0db2666e258
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-08 21:23:44 -0700

    Add components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="container mx-auto mt-12 max-w-screen-xl bg-white p-4 md:p-8 lg:p-10">
+      <div className="text-center">
+        <Link
+          to="/"
+          className="flex items-center justify-center text-2xl font-semibold text-gray-900"
+        >
+          <SparklesIcon className="me-2 h-8 w-8 text-primary-600 hover:cursor-pointer hover:text-primary-800" />
+          Hendry
+        </Link>
+        <p className="my-6 text-gray-500">
+          The capacity to learn is a gift; the ability to learn is a skill; the
+          willingness to learn is a choice
+        </p>
+        <ul className="mb-6 flex flex-wrap items-center justify-center space-x-4 text-gray-900 dark:text-white">
+          <li>
+            <Link to="/about" className="hover:underline">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/portfolio" className="hover:underline">
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="hover:underline">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <span className="text-sm text-gray-500">
+          © {year}{" "}
+          <a href="#" className="hover:underline">
+            Hendry Widyanto
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+import { Link, NavLink } from "react-router-dom";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import clsx from "clsx";
+import { useState } from "react";
+
+export default function Navbar() {
+  const [menuHidden, setMenuHidden] = useState(true);
+  const styleMenu = clsx(menuHidden && "hidden", "w-full md:block md:w-auto");
+  const styleLink = clsx(
+    "block rounded px-3 py-2 text-lg hover:bg-blue-700 hover:text-white dark:text-white md:bg-transparent md:p-0 md:text-black md:hover:bg-transparent md:hover:text-primary-500",
+  );
+
+  const changeMenuHidden = () => {
+    setMenuHidden(!menuHidden);
+  };
+
+  return (
+    <nav className="border-b-2 border-gray-200 bg-white shadow-sm">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-3">
+        <Link to="/" className="flex items-center space-x-4">
+          <SparklesIcon className="h-8 w-8 text-primary-600 hover:cursor-pointer hover:text-primary-800" />
+          <span className="text-2xl font-semibold">Hendry Widyanto</span>
+        </Link>
+        <button
+          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
+          onClick={() => changeMenuHidden()}
+        >
+          <span className="sr-only">Open main menu</span>
+          <Bars3Icon className="h-10 w-10 text-primary-600 hover:cursor-pointer hover:text-primary-800" />
+        </button>
+        <div className={styleMenu} id="navbar-default">
+          <ul className="mt-4 flex flex-col rounded-xl border-2 border-gray-300 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+            <li>
+              <NavLink
+                to="/"
+                className={styleLink}
+                onClick={() => changeMenuHidden()}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/portfolio"
+                className={styleLink}
+                onClick={() => changeMenuHidden()}
+              >
+                Portfolio
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={styleLink}
+                onClick={() => changeMenuHidden()}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
