ea11a110691062b8410a1cbd8b1ab15cf4e069f7
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-13 20:28:01 -0700

    Update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,28 @@ import { Link, NavLink } from "react-router-dom";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import clsx from "clsx";
-import { useState } from "react";
+import { useState, useRef } from "react";
+// import ReactDOM from "react-dom";
 
 export default function Navbar() {
   const [menuHidden, setMenuHidden] = useState(true);
-  const styleMenu = clsx(menuHidden && "hidden", "w-full md:block md:w-auto");
+  const newRef = useRef(null);
+
+  // useEffect(() => {
+  //   document.addEventListener("mousedown", (e) => handleOutsideClick(e));
+  //   return () => {
+  //     document.removeEventListener("mousedown", (e) => handleOutsideClick(e));
+  //   };
+  // });
+
+  // const handleOutsideClick = (e: MouseEvent) => {
+  //   console.log(newRef.current)
+  // };
+
+  const styleMenu = clsx(
+    menuHidden && "hidden",
+    "absolute left-0 top-0 z-50 mt-20 w-full bg-white px-4 md:static md:mt-0 md:block md:w-auto",
+  );
   const styleLink = clsx(
     "block rounded-xl px-3 py-2 text-lg hover:bg-blue-700 hover:text-white md:bg-transparent md:p-0 md:text-black md:hover:bg-transparent md:hover:text-primary-500",
   );
@@ -16,8 +33,8 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="border-b-2 border-gray-200 bg-white shadow-sm">
-      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-3">
+    <nav className="relative border-b-2 border-gray-200 bg-white shadow-sm">
+      <div className=" mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-3">
         <Link to="/" className="flex items-center space-x-4">
           <SparklesIcon className="h-8 w-8 text-primary-600 hover:cursor-pointer hover:text-primary-800" />
           <span className="text-2xl font-semibold">Hendry Widyanto</span>
@@ -29,8 +46,8 @@ export default function Navbar() {
           <span className="sr-only">Open main menu</span>
           <Bars3Icon className="h-10 w-10 text-primary-600 hover:cursor-pointer hover:text-primary-800" />
         </button>
-        <div className={styleMenu} id="navbar-default">
-          <ul className="mt-4 flex flex-col rounded-xl border border-gray-300 p-4 font-medium shadow-lg md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:shadow-none md:dark:bg-gray-900">
+        <div className={styleMenu} id="navbar-default" ref={newRef}>
+          <ul className="flex flex-col rounded-xl border border-gray-300 p-4 font-medium shadow-lg md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:shadow-none md:dark:bg-gray-900">
             <li>
               <NavLink
                 to="/"
