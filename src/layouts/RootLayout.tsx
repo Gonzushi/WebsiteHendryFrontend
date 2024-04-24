052408b194500b64e8fd2cd6da6103146603a2cb
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-24 13:11:19 -0700

    Add GA

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -2,25 +2,28 @@ import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactGA from "react-ga4";
 import { useLocation } from "react-router-dom";
 
 export default function RootLayout() {
   const location = useLocation();
-  // console.log(location)
+  const [statusGA, setStatusGA] = useState(false);
   useEffect(() => {
     ReactGA.send({
       hitType: "pageview",
       page: location.pathname + location.search,
     });
+    setStatusGA(true);
   }, [location]);
 
   return (
     <div>
       <ScrollRestoration />
 
-      <div className="text-center font-semibold">{location.pathname + location.search}</div>
+      <div className="text-center font-semibold">
+        {location.pathname + location.search + " " + statusGA}
+      </div>
 
       <Navbar />
 
