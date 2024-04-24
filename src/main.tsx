167123af2f2fb2a59191819935c88679c20c5977
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-24 10:06:18 -0700

    Add google analytic

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RouterDOM from "./router";
+// import ReactGA from 'react-ga'
 import "./index.css";
+
+// ReactGA.initialize('G-07QF48SE6H');
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
