9a601bb73956fcfbdb5ce7e1491b9c8b9fc73083
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-04-25 17:10:08 -0700

    Add personal anlytic

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import RouterDOM from "./router";
 import "./index.css";
 
-// import ReactGA from "react-ga4";
-// ReactGA.initialize("G-07QF48SE6H");
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterDOM />,
